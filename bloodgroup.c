7d391b032abdfad96f56b9f369ff2fb3121bc379
Ruj <rutkiller@gmail.com>
2019-11-20 21:34:58 +0700

    Chage answer100 and dog name

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[50], G[5], answer[10];
+{ char Name[50], G[5], answer[100];
   int i;
   do
   {
-    printf("What is your name? ");
+    printf("What is your dog name? ");
     scanf("%s", Name);
     do
     {
